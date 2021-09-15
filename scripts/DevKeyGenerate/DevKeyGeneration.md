826d047169f19394f04766c06f649bce81e9c435
Rick Feng <rickfeng99@gmail.com>
2021-09-14 21:58:48 -0700

    Dev Key and Dev table is now available

---
+++ scripts/DevKeyGenerate/DevKeyGeneration.md
@@ -0,0 +1,2 @@
+# Funtionailty
+## This is a script auto-generate 7 digit Dev ID and store it in a csv file
---
+++ scripts/DevKeyGenerate/DevKeyGeneration.py
@@ -0,0 +1,26 @@
+from random import randint
+import math
+import csv
+
+isFull = False
+
+DevID = set()
+DevKey = list()
+DevKey.append("Data448ID")
+
+while(isFull == False):
+    randomID = randint(1000000, 9999999)
+
+    DevID.add(randomID)
+
+    if(len(DevID) == 161):
+        isFull = True
+
+for id in DevID:
+    DevKey.append(str(id))
+
+with open("DevKey.csv", "w") as f:
+    writer = csv.writer(f,lineterminator='\n')
+    for id in DevKey:
+        writer.writerow([id])
+f.close()
