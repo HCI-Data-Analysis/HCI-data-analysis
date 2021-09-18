a639b97a32d364f4ddf04ec8e1057bc4e78c85ac
opeyem1a <opeyadeyemi@gmail.com>
2021-09-18 15:50:59 -0700

    Fixed encoder issue by changing the coding pattern

--- scripts/convert_impression_survey/convert_impression_survey.py
+++ scripts/convert_impression_survey/convert_impression_survey.py
@@ -1,6 +1,7 @@
 import pandas as pd
-from util import encoder
+from util import Encoder, EncoderException
 
+KEY_PATH = '../../keys/Key.csv'
 SURVEY_PATH = '../../data/Your_Impression_of_HCI__10_min_Header.csv'
 survey_df = pd.read_csv(SURVEY_PATH)
 
@@ -9,9 +10,10 @@ def convert_impression_survey():
     converted_survey_df = survey_df.drop(columns=['name'])
     for i, row in converted_survey_df.iterrows():
         try:
+            encoder = Encoder(KEY_PATH)
             student_id = encoder.encode(row['id'])
             converted_survey_df.at[i, 'id'] = student_id
-        except encoder.EncoderException:
+        except EncoderException:
             converted_survey_df.drop(i, inplace=True)
     print(converted_survey_df)
 
