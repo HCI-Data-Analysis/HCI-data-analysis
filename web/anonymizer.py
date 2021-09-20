cfcc7d3a4578bb37265b3152337978261eebee2a
Mathew de Vin <mathewdevin03@gmail.com>
2021-09-20 10:57:40 -0700

    Start adding functionality
     - Anonymize surveys

--- web/anonymizer.py
+++ web/anonymizer.py
@@ -1,14 +1,21 @@
 import eel
 from utils.decodeBase64 import generate_decoded_file
+from scripts.key_generator.key_generator import generate_key
+from scripts.survey_anonymizers.survey_anonymizer import survey_anonymize
+from scripts.zip_file_anonymizer.zip_file_anonymizer import zip_anonymize
 
 
 eel.expose(generate_decoded_file)
+eel.expose(generate_key)
+eel.expose(survey_anonymize)
+eel.expose(zip_anonymize)
 
 
-def init_eel():
+def start_eel():
     eel.init('anonymizer')
     eel.start('main.html')
 
 
 if __name__ == '__main__':
-    init_eel()
+    start_eel()
+    print('closing')
