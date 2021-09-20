38be372a83c356463f094f6c9ae1a27401a43c06
opeyem1a <opeyadeyemi@gmail.com>
2021-09-20 10:17:53 -0700

    code formatting improvements

--- scripts/convert_zip_file/convert_zip_file_id.py
+++ scripts/convert_zip_file/convert_zip_file_id.py
@@ -1,13 +1,16 @@
-import sys, os, inspect
+import inspect
+import os
+import sys
 
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-maindir = os.path.dirname(parentdir)
-sys.path.insert(0, maindir)
+from util import Encoder, EncoderException
 
-from util import *
+current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parent_dir = os.path.dirname(current_dir)
+main_dir = os.path.dirname(parent_dir)
+sys.path.insert(0, main_dir)
 
 KEY_PATH = '../../keys/Key.csv'
+
 
 def convert_zip_file():
     for subdir, dirs, files in os.walk(r'../../data'):
@@ -18,28 +21,28 @@ def convert_zip_file():
                 real_student_information = filename.split("_")
                 try:
                     encoder = Encoder(KEY_PATH)
-                    if (len(real_student_information) == 3):
+                    if len(real_student_information) == 3:
                         canvas_id = int(real_student_information[1])
                         half_key = encoder.encode(canvas_id)
                         id = str(half_key)
-                        os.rename(filepath,'../../data/' + id +'.zip')
-                    elif (len(real_student_information) == 4):
+                        os.rename(filepath, '../../data/' + id + '.zip')
+                    elif len(real_student_information) == 4:
                         canvas_id = int(real_student_information[1])
                         assignment_name = str(real_student_information[3])
                         half_key = encoder.encode(canvas_id)
                         id = str(half_key)
-                        os.rename(filepath,'../../data/' + id + '_' + assignment_name)
-                    elif (len(real_student_information) == 5):
+                        os.rename(filepath, '../../data/' + id + '_' + assignment_name)
+                    elif len(real_student_information) == 5:
                         canvas_id = int(real_student_information[2])
                         assignment_name = str(real_student_information[4])
                         half_key = encoder.encode(canvas_id)
                         id = str(half_key)
-                        os.rename(filepath,'../../data/' + id + '_' + assignment_name)
+                        os.rename(filepath, '../../data/' + id + '_' + assignment_name)
                     else:
                         print("unsuitable format")
                 except EncoderException as e:
                     print(e)
 
+
 if __name__ == "__main__":
     convert_zip_file()
-
