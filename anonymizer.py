c4e9baceb077b993cd79913299dd67d816500c82
opeyem1a <opeyadeyemi@gmail.com>
2021-09-24 05:30:19 -0700

    + many things

--- anonymizer.py
+++ anonymizer.py
@@ -7,7 +7,7 @@ from scripts.gradebook_anonymizers.gradebook_anonymizer import gradebook_anonymi
 from scripts.key_generator.key_generator import generate_key
 from scripts.survey_anonymizers.survey_anonymizer import survey_anonymize
 from scripts.zip_file_anonymizer.zip_file_anonymizer import zip_anonymize
-from util.decodeBase64 import generate_decoded_file
+from util.decode_base_64 import generate_decoded_file
 
 eel.expose(generate_decoded_file)
 eel.expose(generate_key)
