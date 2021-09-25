e2ed12c676c9f5eb236db437de3124dc76e7e733
opeyem1a <opeyadeyemi@gmail.com>
2021-09-25 14:57:55 -0700

    + add support for both impressions surveys

--- anonymize.py
+++ anonymize.py
@@ -12,8 +12,10 @@ BACKGROUND_SURVEY_PATH = 'raw_data/341_Background_Survey_Header.csv'
 BACKGROUND_EXPORT_PATH = 'data/background_survey.csv'
 GRADE_BOOK_CSV_PATH = 'raw_data/2021-09-13T1128_Grades-COSC_341_COSC_541_101_2020W.csv'
 GRADE_BOOK_EXPORT_PATH = 'data/grade_book.csv'
-IMPRESSION_SURVEY_PATH = 'raw_data/Your_Impression_of_HCI__10_min_Header.csv'
+IMPRESSION_SURVEY_PATH = 'raw_data/Your_Impression_of_HCI__10_min_Header-First.csv'
 IMPRESSION_EXPORT_PATH = 'data/impression_survey.csv'
+IMPRESSION_SURVEY_PATH_2 = 'raw_data/Your_Impression_of_HCI__10_min_Header-Second.csv'
+IMPRESSION_EXPORT_PATH_2 = 'data/impression_survey_2.csv'
 TA_SURVEY_PATH = 'raw_data/341_TA_resources_survey_W2020T2_September_13_2021_12.36.csv'
 TA_EXPORT_PATH = 'data/ta_survey.csv'
 
@@ -22,6 +24,10 @@ if __name__ == '__main__':
     # Convert Impression Survey
     survey = convert_survey(pd.read_csv(IMPRESSION_SURVEY_PATH), encoder)
     survey.to_csv(IMPRESSION_EXPORT_PATH, index=False)
+
+    # Convert Impression Survey 2
+    survey = convert_survey(pd.read_csv(IMPRESSION_SURVEY_PATH_2), encoder)
+    survey.to_csv(IMPRESSION_EXPORT_PATH_2, index=False)
 
     # Convert Background Survey
     survey = convert_survey(pd.read_csv(BACKGROUND_SURVEY_PATH), encoder)
