d14205a35a26f8075938aba335ae586885860f89
Carson Ricca <59884796+carson-ricca@users.noreply.github.com>
2021-09-26 18:50:07 -0700

    Prepare Background Survey Properly

---
+++ scripts/clustering/background_survey_clustering.md
@@ -0,0 +1,21 @@
+# Background Survey Clustering
+
+Contains a method called `export_to_csv()` that will take the background survey data and convert it to a csv that is
+prepared for clustering. 
+
+The questions in the background survey were all under a single column. This script moves all
+the questions into their own individual columns.
+
+The following fields were also removed to simplify the dataset with only the required data for clustering.
+
+- `section`
+- `section_id`
+- `subimtted`
+- `attempt`
+- `Course Section`
+- `Meeting Times`
+- `Ideal Teammates`
+- `Teammate to Avoid`
+- `n correct`
+- `n incorrect`
+- `score`
---
+++ scripts/clustering/background_survey_clustering/clustering_model.py
@@ -0,0 +1,52 @@
+import re
+
+import sklearn as sk
+import numpy as np
+import pandas as pd
+import os
+
+BACKGROUND_SURVEY_SCHEMA = "../../../data/background_survey_schema.csv"
+BACKGROUND_SURVEY_DATA = "../../../data/background_survey.csv"
+OUTPUT_DIRECTORY = "../../../data"
+FILE_NAME = "processed_background_survey"
+
+
+# Prepares and cleans the dataframe so it is possible to use it for clustering.
+def prepare_df():
+    df = pd.read_csv(BACKGROUND_SURVEY_DATA)
+    df = df.drop(df.columns[1:13], axis=1).drop(df.columns[14:19], axis=1).dropna()
+    questions = list(df.columns)[1]
+    new_columns = get_columns(questions)
+
+    row_index = 1
+    for row in df.itertuples():
+        answers = row[2].split(",")
+        column_index = 0
+        for column in new_columns:
+            if column_index < len(answers):
+                df.at[row_index, column] = answers[column_index]
+            column_index += 1
+        row_index += 1
+    return df
+
+
+# Exports the processed dataframe into a csv.
+def export_to_csv():
+    df = prepare_df()
+    df = df.drop(df.columns[1], axis=1)
+    output_dir = os.path.join(OUTPUT_DIRECTORY, FILE_NAME + '.csv')
+    df.to_csv(output_dir, index=False)
+
+
+# This separates the questions into a list so they can be used to update the columns in the dataframe.
+def get_columns(questions):
+    new_columns = []
+    for question in questions.splitlines():
+        substring = re.search(r"\[(.*?)]", question)
+        if substring:
+            new_columns.append(substring.group(1).replace("_", " "))
+    return new_columns
+
+
+if __name__ == "__main__":
+    export_to_csv()
