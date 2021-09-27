c881ceff47134936283ff6874516946e93bdc62a
Rick Feng <rickfeng99@gmail.com>
2021-09-26 21:17:15 -0700

    Update course_performace_analysis.py

--- scripts/course_performace_analysis/course_performace_analysis.py
+++ scripts/course_performace_analysis/course_performace_analysis.py
@@ -6,6 +6,17 @@ import seaborn as sns
 
 def course_performace_analysis(gradebook_path):
     file = pd.read_csv(gradebook_path)
+    w_2015 = 80.99
+    w_2016 = 79.93
+    w_2017 = 78.70
+    w_2018 = 85.97
+    w_2019 = 83.81
+    w_2020 = 90.80
+    mean = file['Final Score'].mean()
+    quantile_25percent = file['Final Score'].quantile(.25)
+    median = file['Final Score'].median()
+    quantile_75percent = file['Final Score'].quantile(.75)
+    print(quantile_25percent, median, quantile_75percent, mean)
     # sns.distplot(file['Final Score'], hist=True, kde=True, 
     #          bins=int(180/5), color = 'darkblue', 
     #          hist_kws={'edgecolor':'black'},
@@ -15,7 +26,7 @@ def course_performace_analysis(gradebook_path):
     plt.title('Course performace')
     plt.xlabel('Grade')
     plt.ylabel('amount of student')
-    plt.show()
+    #plt.show()
     
     
     
