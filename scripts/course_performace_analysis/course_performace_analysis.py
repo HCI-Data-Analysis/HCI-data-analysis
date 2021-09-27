3ffd7349c2d9cf4cb5a0d16f4d7b5d26c43ce314
Rick Feng <rickfeng99@gmail.com>
2021-09-26 22:03:46 -0700

    Update course_performace_analysis.py

--- scripts/course_performace_analysis/course_performace_analysis.py
+++ scripts/course_performace_analysis/course_performace_analysis.py
@@ -12,21 +12,21 @@ def course_performace_analysis(gradebook_path):
     w_2018 = 85.97
     w_2019 = 83.81
     w_2020 = 90.80
+    
     mean = file['Final Score'].mean()
+    standard_dev = file['Final Score'].std
     quantile_25percent = file['Final Score'].quantile(.25)
     median = file['Final Score'].median()
     quantile_75percent = file['Final Score'].quantile(.75)
-    print(quantile_25percent, median, quantile_75percent, mean)
-    # sns.distplot(file['Final Score'], hist=True, kde=True, 
-    #          bins=int(180/5), color = 'darkblue', 
-    #          hist_kws={'edgecolor':'black'},
-    #          kde_kws={'linewidth': 4})
+    
+    print("25% quantile: " + str(quantile_25percent)," Median: " + str(median), " 75% quantile: " + str(quantile_75percent), " Mean: " + str(mean))
+    
     sns.displot(file['Final Score'], kde=True, bins = 30)
     
     plt.title('Course performace')
     plt.xlabel('Grade')
     plt.ylabel('amount of student')
-    #plt.show()
+    plt.show()
     
     
     
