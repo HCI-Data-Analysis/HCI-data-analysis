384797816939053b4c988d2377124d3b12c15937
Mathew de Vin <mathewdevin03@gmail.com>
2021-09-29 14:58:50 -0700

    Anonymize submission retrieval

--- scripts/canvas_submission_retrieval/canvas_submission_retrieval.py
+++ scripts/canvas_submission_retrieval/canvas_submission_retrieval.py
@@ -4,6 +4,8 @@ import os
 
 from canvasapi import Canvas
 
+from util import Encoder
+
 
 class DateTimeEncoder(json.JSONEncoder):
     def default(self, z):
@@ -13,18 +15,22 @@ class DateTimeEncoder(json.JSONEncoder):
             return super().default(z)
 
 
-def canvas_submission_retrieval(course_id, access_token, export_dir):
+def canvas_submission_retrieval(course_id, access_token, export_dir, encoder: Encoder):
     canvas = Canvas('https://canvas.ubc.ca', access_token)
     assignments = canvas.get_course(course_id).get_assignments()
-    for assignment in assignments:
+    temp_ta = {}
+    for index, assignment in enumerate(assignments, start=1):
         submissions = assignment.get_submissions()
-        filename = str(course_id) + "_" + str(assignment.id)
-        output_dir = os.path.join(export_dir, filename + '.json')
+        filename = str(index) + "_" + str(assignment.id)
+        output_dir = os.path.join(export_dir, course_id, filename + '.json')
         with open(output_dir, "w") as f:
             json_submissions = []
             for submission in submissions:
                 submission_dict = submission.__dict__
-                submission_dict.pop('_requester')
+                for key in ['_requester', 'preview_url', 'course_id']:
+                    submission_dict.pop(key)
+                submission_dict['grader_id'] = temp_ta.setdefault(submission_dict['grader_id'], len(temp_ta) + 1)
+                submission_dict['user_id'] = encoder.encode(canvas_id=submission_dict['user_id'])
                 json_submissions.append(json.dumps(submission_dict, indent=4, cls=DateTimeEncoder))
             f.write('[' + ','.join(json_submissions) + ']')
 
--- submission_retrieval.py
+++ submission_retrieval.py
@@ -1,10 +1,24 @@
-from scripts import canvas_submission_retrieval
+import os
+
+from scripts import canvas_submission_retrieval, generate_key
 import sys
 
-EXPORT_DIR = 'data/canvas_submission'
+from util import Encoder
+
+KEY_DIR = 'keys'
+KEY_EXPORT_FILENAME = 'Key'
+
+EXPORT_DIR = os.path.join('data', 'canvas_submission')
 CANVAS_ACCESS_TOKEN = sys.argv[1]
 CANVAS_COURSE_ID = sys.argv[2]
+GRADE_BOOK_CSV_PATH = 'raw_data/2021-09-13T1128_Grades-COSC_341_COSC_541_101_2020W.csv'
 
 if __name__ == '__main__':
-    # submission retrieval
-    canvas_submission_retrieval(CANVAS_COURSE_ID, CANVAS_ACCESS_TOKEN, EXPORT_DIR)
+
+    key_path = generate_key(GRADE_BOOK_CSV_PATH, KEY_DIR, KEY_EXPORT_FILENAME)
+    encoder = Encoder(key_path)
+
+    os.mkdir(EXPORT_DIR)
+    os.mkdir(os.path.join(EXPORT_DIR, CANVAS_COURSE_ID))
+
+    canvas_submission_retrieval(CANVAS_COURSE_ID, CANVAS_ACCESS_TOKEN, EXPORT_DIR, encoder)
