7ca6a56cce3d1d35d72b94ed6f6171d666cfc8b0
Carson Ricca <59884796+carson-ricca@users.noreply.github.com>
2021-09-29 12:52:02 -0700

    Add some more comments

--- cluster_background.py
+++ cluster_background.py
@@ -10,6 +10,11 @@ OUTPUT_DIRECTORY = "../../../data/processed"
 STUDENT_GROUP_OUTPUT_DIRECTORY = "../../../data/student_group"
 
 if __name__ == "__main__":
+    # Pre-process background survey data.
     clean_background_survey(BACKGROUND_SURVEY_DATA, OUTPUT_DIRECTORY)
+
+    # Prepare data for clustering.
     prepare_data_background(PROCESSED_BACKGROUND_SURVEY_DATA, BACKGROUND_SURVEY_SCHEMA, OUTPUT_DIRECTORY)
+
+    # Execute clustering on the data and display graphs.
     cluster_survey(BACKGROUND_CLUSTER_DATA, STUDENT_GROUP_OUTPUT_DIRECTORY)
--- cluster_impressions.py
+++ cluster_impressions.py
@@ -8,5 +8,8 @@ HCI_CLUSTER_DATA = "../../../data/processed/for_clustering_impression_survey1.cs
 STUDENT_GROUP_OUTPUT_DIRECTORY = "../../../data/student_group"
 
 if __name__ == "__main__":
+    # Prepare data for clustering.
     prepare_data_hci(HCI_SURVEY_DATA, HCI_SURVEY_SCHEMA, OUTPUT_DIRECTORY)
+
+    # Execute clustering on the data and display graphs.
     cluster_survey(HCI_CLUSTER_DATA, STUDENT_GROUP_OUTPUT_DIRECTORY)
