a4c1d6ee1d6809dbe689daa531ef04d88d4bea0a
Carson Ricca <59884796+carson-ricca@users.noreply.github.com>
2021-09-29 11:31:12 -0700

    Update __init__.py

--- schemas/__init__.py
+++ schemas/__init__.py
@@ -2,4 +2,3 @@ from .gradebook import GradeBookSchema
 from .key import KeySchema
 from .raw import RawGradeBookSchema, RawSurveySchema, RawTASurveySchema
 from .surveys import SurveySchema, TASurveySchema
-from .data_files import DataFilesSchema
