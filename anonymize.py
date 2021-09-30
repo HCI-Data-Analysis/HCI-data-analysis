ee37cbd6d707d1274d3b0805640881df0fe3f6ee
opeyem1a <opeyadeyemi@gmail.com>
2021-09-30 13:37:14 -0700

    + updated anonymize.py to generate files needed + mkdir_if_not_exists() + minor file path fixes

--- anonymize.py
+++ anonymize.py
@@ -1,5 +1,5 @@
 from scripts import generate_key, gradebook_anonymize, survey_anonymize, anonymize_ta_survey
-from util import Encoder
+from util import Encoder, mkdir_if_not_exists
 
 KEY_DIR = 'keys'
 KEY_EXPORT_FILENAME = 'Key'
@@ -10,13 +10,14 @@ BACKGROUND_EXPORT_FILENAME = 'background_survey'
 GRADE_BOOK_CSV_PATH = 'raw_data/2021-09-13T1128_Grades-COSC_341_COSC_541_101_2020W.csv'
 GRADE_BOOK_EXPORT_FILENAME = 'grade_book'
 IMPRESSION_SURVEY_PATH = 'raw_data/Your_Impression_of_HCI__10_min_Header-First.csv'
-IMPRESSION_EXPORT_FILENAME = 'impression_survey'
+IMPRESSION_EXPORT_FILENAME = 'impression_survey1'
 IMPRESSION_SURVEY_PATH_2 = 'raw_data/Your_Impression_of_HCI__10_min_Header-Second.csv'
-IMPRESSION_EXPORT_FILENAME_2 = 'impression_survey_2'
+IMPRESSION_EXPORT_FILENAME_2 = 'impression_survey2'
 TA_SURVEY_PATH = 'raw_data/341_TA_resources_survey_W2020T2_September_13_2021_12.36.csv'
 TA_EXPORT_FILENAME = 'ta_survey'
 
 if __name__ == '__main__':
+    mkdir_if_not_exists('data/anonymized')
     # Generate Key file
     key_path = generate_key(GRADE_BOOK_CSV_PATH, KEY_DIR, KEY_EXPORT_FILENAME)
     encoder = Encoder(key_path)
--- util/__init__.py
+++ util/__init__.py
@@ -1,2 +1,3 @@
 from .encoder import Encoder, EncoderException
 from .cluster import inertia_graph, kmeans_clustering, get_groups
+from .file import mkdir_if_not_exists
