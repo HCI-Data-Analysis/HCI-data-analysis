d97b6db6980211f7adbbd6a08406b94149a75a1d
nononovia <44279056+nononovia@users.noreply.github.com>
2021-09-30 00:59:38 -0700

    Change column names in survey schemas to be more detailed. Update schemas/cluster.py with above changes

--- schemas/cluster.py
+++ schemas/cluster.py
@@ -1,6 +1,6 @@
 class ClusterSchema:
     QUESTION = 'question'
-    COL_NUM_1 = 'col num 1'
-    COL_NUM_2 = 'col num 2'
-    CATEGORY = 'category'
-    POSITIVE_NEGATIVE = 'positive/negative'
+    COL_NUM_1 = 'column number of the question in survey data'
+    COL_NUM_2 = 'column number of the question in second survey'
+    CATEGORY = 'question category'
+    POSITIVE_NEGATIVE = 'positive/negative phrasing of the question'
