cd89a313cee0c367c4ac0563622c34dcf09cb8a0
Rick Feng <rickfeng99@gmail.com>
2021-10-01 13:37:03 -0700

    Update course_performace_analysis.py

--- scripts/course_performace_analysis/course_performace_analysis.py
+++ scripts/course_performace_analysis/course_performace_analysis.py
@@ -6,37 +6,34 @@ import seaborn as sns
 def course_performance_analysis(gradebook_path):
     file = pd.read_csv(gradebook_path)
     col = 'Overall Final Score'
-    w_2015 = 80.99
-    w_2016 = 79.93
-    w_2017 = 78.70
-    w_2018 = 85.97
-    w_2019 = 83.81
+    w_2016_avg, w_2016_std = 79.93, 7.9
+    w_2017_avg, w_2017_std = 78.70, 16.2
+    w_2018_avg, w_2018_std = 85.97, 8.8
+    w_2019_avg, w_2019_std = 83.81, 10.0
+    w_2020_avg, w_2020_std = 90.80, 10.5
 
     mean = file[col].mean()
-    standard_dev = file[col].std
+    standard_dev = file[col].std()
     quantile_25percent = file[col].quantile(.25)
     median = file[col].median()
     quantile_75percent = file[col].quantile(.75)
 
     print("25% quantile: " + str(quantile_25percent), " Median: " + str(median),
-          " 75% quantile: " + str(quantile_75percent), " Mean: " + str(mean))
+          " 75% quantile: " + str(quantile_75percent), " Mean: " + str(mean), " Standard Deviation: " + str(standard_dev))
+    
+    df_year_score = pd.DataFrame({'Average Score': [w_2016_avg,w_2017_avg,w_2018_avg,w_2019_avg,w_2020_avg],'Standard Deviation': [w_2016_std,w_2017_std,w_2018_std,w_2019_std,w_2020_std] },columns=['Average Score','Standard Deviation'])
+    df_year_score.index = ['Winter 2016', 'Winter 2017', 'Winter 2018', 'Winter 2019', 'Winter 2020']
+    df_year_score.plot(kind = "barh",y = 'Average Score', legend = False,
+            title = "COSC 341 Averge Scores Years Compare", xerr = "Standard Deviation")
+    
+    fig, ax = plt.subplots()
+    
+    sns.displot(file[col], kde=True, ax=ax, bins=30, height=5, aspect=1.5).set(title='Course performance', xlabel='Grade', ylabel='Number of students')
 
-    sns.displot(file[col], kde=True, bins=30, height=5, aspect=1.5)
-
-    plt.axvline(x=w_2015, label='2015: 80.99%', color='r')
-    plt.axvline(x=w_2016, label='2016: 79.93%', color='g')
-    plt.axvline(x=w_2017, label='2017: 78.70%', color='b')
-    plt.axvline(x=w_2018, label='2018: 85.97%', color='c')
-    plt.axvline(x=w_2019, label='2019: 83.81%', color='m')
-    plt.axvline(x=mean, label=f'2020: {format(mean, ".2f")}%', color='y')
-    plt.legend()
-
-    plt.title('Course performance')
-    plt.xlabel('Grade')
-    plt.ylabel('Number of students')
-    plt.xticks(ticks=range(40, 110, 5), labels=[f'{i}%' for i in range(40, 110, 5)])
-    plt.yticks(ticks=range(0, 25, 3), labels=range(0, 25, 3))
-    plt.tight_layout()
+    ax.set_xticks(range(40, 110, 5))
+    ax.set_xticklabels([f'{i}%' for i in range(40, 110, 5)])
+    ax.set_yticks(range(0, 25, 3))
+    fig.tight_layout()
     plt.show()
 
 
