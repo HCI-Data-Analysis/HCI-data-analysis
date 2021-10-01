4fda77d1f55d5350b4d0d9e665a1c0866c71f851
Rick Feng <rickfeng99@gmail.com>
2021-10-01 14:01:45 -0700

    Update course_performace_analysis.py

--- scripts/course_performace_analysis/course_performace_analysis.py
+++ scripts/course_performace_analysis/course_performace_analysis.py
@@ -6,23 +6,26 @@ import seaborn as sns
 def course_performance_analysis(gradebook_path):
     file = pd.read_csv(gradebook_path)
     col = 'Overall Final Score'
-    w_2016_avg, w_2016_std = 79.93, 7.9
-    w_2017_avg, w_2017_std = 78.70, 16.2
-    w_2018_avg, w_2018_std = 85.97, 8.8
-    w_2019_avg, w_2019_std = 83.81, 10.0
-    w_2020_avg, w_2020_std = 90.80, 10.5
-
+    
     mean = file[col].mean()
     standard_dev = file[col].std()
     quantile_25percent = file[col].quantile(.25)
     median = file[col].median()
     quantile_75percent = file[col].quantile(.75)
+    
+    
+    w_2016_avg, w_2016_std = 79.93, 7.9
+    w_2017_avg, w_2017_std = 78.70, 16.2
+    w_2018_avg, w_2018_std = 85.97, 8.8
+    w_2019_avg, w_2019_std = 83.81, 10.0
+    w_2020_retrive_avg, w_2020_retrive_std = 90.80, 10.5
+    w_2020_calculated_avg, w_2020_calculated_std = mean, standard_dev
 
     print("25% quantile: " + str(quantile_25percent), " Median: " + str(median),
           " 75% quantile: " + str(quantile_75percent), " Mean: " + str(mean), " Standard Deviation: " + str(standard_dev))
     
-    df_year_score = pd.DataFrame({'Average Score': [w_2016_avg,w_2017_avg,w_2018_avg,w_2019_avg,w_2020_avg],'Standard Deviation': [w_2016_std,w_2017_std,w_2018_std,w_2019_std,w_2020_std] },columns=['Average Score','Standard Deviation'])
-    df_year_score.index = ['Winter 2016', 'Winter 2017', 'Winter 2018', 'Winter 2019', 'Winter 2020']
+    df_year_score = pd.DataFrame({'Average Score': [w_2016_avg,w_2017_avg,w_2018_avg,w_2019_avg,w_2020_retrive_avg, w_2020_calculated_avg],'Standard Deviation': [w_2016_std,w_2017_std,w_2018_std,w_2019_std,w_2020_retrive_std, w_2020_calculated_std] },columns=['Average Score','Standard Deviation'])
+    df_year_score.index = ['Winter 2016', 'Winter 2017', 'Winter 2018', 'Winter 2019', 'Winter 2020 from UBC website', 'Winter 2020 Calculated from Gradebook']
     df_year_score.plot(kind = "barh",y = 'Average Score', legend = False,
             title = "COSC 341 Averge Scores Years Compare", xerr = "Standard Deviation")
     
