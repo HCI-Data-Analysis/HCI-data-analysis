527d6257ebe981b6d501c04ba711e6876a679d54
MtheDV <mathewdevin03@gmail.com>
2021-10-01 13:38:29 -0700

    Add quiz submission download

--- scripts/canvas_submission_retrieval/canvas_submission_retrieval.py
+++ scripts/canvas_submission_retrieval/canvas_submission_retrieval.py
@@ -15,22 +15,35 @@ class DateTimeEncoder(json.JSONEncoder):
             return super().default(z)
 
 
+SUBMISSION_REMOVE_VALUES = {
+    'assignment': ['preview_url', 'course_id'],
+    'quiz': ['course_id', 'html_url', 'result_url', 'validation_token'],
+}
+
+
 def canvas_submission_retrieval(course_id, access_token, export_dir, encoder: Encoder):
     canvas = Canvas('https://canvas.ubc.ca', access_token)
-    assignments = canvas.get_course(course_id).get_assignments()
     temp_ta = {}
-    for index, assignment in enumerate(assignments, start=1):
-        submissions = assignment.get_submissions()
-        filename = str(index) + "_" + str(assignment.id)
+    assignments = canvas.get_course(course_id).get_assignments()
+    download_submission(assignments, temp_ta, export_dir, course_id, encoder, 'assignment')
+    quizzes = canvas.get_course(course_id).get_quizzes()
+    download_submission(quizzes, temp_ta, export_dir, course_id, encoder, 'quiz')
+
+
+def download_submission(submission_parent, ta_dict, export_dir, course_id, encoder, submission_type):
+    for index, parent_type in enumerate(submission_parent, start=1):
+        submissions = parent_type.get_submissions()
+        filename = submission_type + "_" + str(index) + "_" + str(parent_type.id)
         output_dir = os.path.join(export_dir, course_id, filename + '.json')
         with open(output_dir, "w") as f:
             json_submissions = []
             for submission in submissions:
                 submission_dict = submission.__dict__
-                for key in ['_requester', 'preview_url', 'course_id']:
+                submission_dict.pop('_requester')
+                for key in SUBMISSION_REMOVE_VALUES[submission_type]:
                     submission_dict.pop(key)
-                submission_dict['grader_id'] = temp_ta.setdefault(submission_dict['grader_id'], len(temp_ta) + 1)
+                if submission_dict.get('grader_id', None):
+                    submission_dict['grader_id'] = ta_dict.setdefault(submission_dict['grader_id'], len(ta_dict) + 1)
                 submission_dict['user_id'] = encoder.encode(canvas_id=submission_dict['user_id'])
                 json_submissions.append(json.dumps(submission_dict, indent=4, cls=DateTimeEncoder))
             f.write('[' + ','.join(json_submissions) + ']')
-
