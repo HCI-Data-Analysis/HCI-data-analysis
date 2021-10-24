982d1b28c9c1c1b20a6d51d1cdb1efa789d10c0d
Abhineeth Adiraju <abhineeth00@gmail.com>
2021-10-24 15:40:19 -0700

    Cleeaned up graphs and plot both std deviations

--- scripts/performance_by_activity_type/performance_by_activity_type.py
+++ scripts/performance_by_activity_type/performance_by_activity_type.py
@@ -16,11 +16,25 @@ def performance_by_activity_type(file_path):
                           GradeBookSchema.OVERALL_PRE_POST_TESTS_SCORE, GradeBookSchema.OVERALL_MAIN_ACTIVITIES_SCORE,
                           GradeBookSchema.OVERALL_TUTORIAL_ACTIVITIES_SCORE, GradeBookSchema.OVERALL_READING_LOGS_SCORE]
     overall_score_data = gradebook.loc[:, gradebook.columns.isin(overall_score_cols)].replace({0: np.nan})
-    sns.displot(overall_score_data, kind='kde', fill=True)
-    plt.show()
+
     for col in overall_score_cols:
-        sns.displot(overall_score_data[col], kind="kde", fill=True, color='pink')
-        plt.axvline(overall_score_data[col].mean(), label='mean', linestyle=':', color='orange')
-        plt.axvline(overall_score_data[col].std(), label='stdev', linestyle='--', color='purple')
-        plt.legend(loc='upper left')
+        # set up plot
+        mean = overall_score_data[col].mean()
+        stdev = overall_score_data[col].std()
+        figure, ax = plt.subplots()
+
+        # plot using seaborn distplot
+        sns.distplot(overall_score_data[col], color='blue', ax=ax)
+
+        # set the limits of the x-axis
+        ax.set_xlim(0, 120)
+
+        # Plot mean and 2 std deviations on either side
+        plt.axvline(mean, label='mean', linestyle=':', color='red')
+        plt.axvline(mean - stdev, label='standard deviation', linestyle='--', color='purple')
+        plt.axvline(mean + stdev, linestyle='--', color='purple')
+        plt.axvline(mean - 2 * stdev, linestyle='--', color='purple')
+        plt.axvline(mean + 2 * stdev, linestyle='--', color='purple')
+        plt.suptitle('Density Distribution of {}'.format(col))
+        plt.legend()
         plt.show()
