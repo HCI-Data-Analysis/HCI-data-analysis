c72e8df2e9fcae1eb4a4a9e8c37fb9a79b9539fa
Abhineeth Adiraju <abhineeth00@gmail.com>
2021-10-31 17:32:39 -0700

    Plot performance by activity type with labelled graphs (#41)

---
+++ scripts/performance_by_activity_type/performance_by_activity_type.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from schemas import GradeBookSchema
+
+
+def performance_by_activity_type(file_path, n_stddev):
+    """
+    This method will extract data from the gradebook and graph the performance by activity type with plotted mean and standard deviations
+    :param file_path: the path to the gradebook csv.
+    :param n_stddev: the number of standard deviations on either side to be plotted for each graph
+    """
+    gradebook = pd.read_csv(file_path)
+    overall_score_cols = [GradeBookSchema.OVERALL_COURSE_SCORE, GradeBookSchema.OVERALL_PROJECT_SCORE,
+                          GradeBookSchema.OVERALL_PRE_POST_TESTS_SCORE, GradeBookSchema.OVERALL_MAIN_ACTIVITIES_SCORE,
+                          GradeBookSchema.OVERALL_TUTORIAL_ACTIVITIES_SCORE, GradeBookSchema.OVERALL_READING_LOGS_SCORE]
+    overall_score_data = gradebook.loc[:, gradebook.columns.isin(overall_score_cols)].replace({0: np.nan})
+
+    for col in overall_score_cols:
+        # set up plot
+        mean = overall_score_data[col].mean()
+        stdev = overall_score_data[col].std()
+        max = overall_score_data[col].max()
+        figure, ax = plt.subplots()
+
+        # plot using seaborn distplot
+        sns.histplot(overall_score_data[col], ax=ax, kde=True)
+
+        # set the limits of the x-axis
+        ax.set_xlim(0, max)
+
+        # Plot mean and n_stddev std deviations on either side
+        plt.axvline(mean, label='mean', linestyle=':', color='red')
+        for i in range(1, n_stddev + 1):
+            plt.axvline(mean - i * stdev, label='standard deviation', linestyle='--', color='purple')
+            plt.axvline(mean + i * stdev, linestyle='--', color='purple')
+
+        plt.suptitle('Density Distribution of {}'.format(col))
+        plt.legend()
+        plt.show()
