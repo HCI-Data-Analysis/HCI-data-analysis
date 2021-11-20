8febf0975ba0cab21f245652f8507886bd4533d0
Carson Ricca <59884796+carson-ricca@users.noreply.github.com>
2021-11-20 11:11:13 -0800

    Update file.py

--- util/file.py
+++ util/file.py
@@ -5,5 +5,6 @@ def mkdir_if_not_exists(path: str, multiple_directories=False) -> str:
     if not os.path.exists(path):
         if not multiple_directories:
             os.mkdir(path)
-        os.makedirs(path)
+        else:
+            os.makedirs(path)
     return path
