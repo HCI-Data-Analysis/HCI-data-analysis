bc10cc3ec441e43186e5bb9c28cd53b9932c7e51
opeyem1a <opeyadeyemi@gmail.com>
2021-11-29 03:45:18 -0800

    + global util file with normalize method (for now)

--- util/__init__.py
+++ util/__init__.py
@@ -5,3 +5,4 @@ from .const import KEY_PATH, MODULE_PARAGRAPHS_OUTPUT_FILEPATH
 from .data_cleaner import keep_latest_survey_attempt
 from .reading_logs import ReadingLogsData, page_reading_duration, module_reading_duration, get_text_difficulty_index
 from .plots import set_plot_settings
+from .util import normalize
