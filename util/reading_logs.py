e89b7204b60f8cf9fa0492751a10f7d481f11238
opeyem1a <opeyadeyemi@gmail.com>
2021-11-28 22:51:02 -0800

    + added docstrings

--- util/reading_logs.py
+++ util/reading_logs.py
@@ -22,6 +22,17 @@ class ReadingLogsData:
 
     def page_reading_speed(self, module_num: int, page_num: int, data448_id: int = None,
                            adjust_for_difficulty: bool = None) -> float:
+        """
+        Retrieves the reading speed for a page. If given a data448_id, only retrieves that student's reading speed
+        for the page. Without a data448_id, retrieves the average reading speed of that page. If desired,
+        this reading speed can be adjusted to factor in difficulty.
+
+        :param module_num: The module number
+        :param page_num: The page number
+        :param data448_id: A student's Data 448 id
+        :param adjust_for_difficulty: Whether or not to normalize this reading speed for difficulty
+        :return: a float representing reading speed in word per minute (WPM)
+        """
         paragraph_list = self.get_paragraph_list(module_num, page_num)
 
         num_words = len(' '.join(paragraph_list).split(' '))
@@ -35,6 +46,17 @@ class ReadingLogsData:
 
     def module_reading_speed(self, module_num: int, data448_id: int = None,
                              adjust_for_difficulty: bool = None) -> float:
+        """
+        Retrieves the reading speed for a module. If given a data448_id, only retrieves that student's reading speed
+        for the page. Without a data448_id, retrieves the average reading speed of that page. If desired,
+        this reading speed can be adjusted to factor in difficulty. In all cases, the returned value is the average
+        reading speed from all pages within the module
+
+        :param module_num: The module number
+        :param data448_id: A student's Data 448 id
+        :param adjust_for_difficulty: Whether or not to normalize this reading speed for difficulty
+        :return: a float representing reading speed in word per minute (WPM)
+        """
         page_reading_speeds = []
         module_paragraphs_dict = self.module_paragraphs_dict()
         for page_num in module_paragraphs_dict[str(module_num)].keys():
@@ -48,13 +70,14 @@ class ReadingLogsData:
 
 
 def page_reading_duration(module_num: int, page_num: int, data448_id: int = None) -> float:
-    """in minutes, average of all students"""
+    """Returns the page reading duration in minutes. Average of all students unless given a data_448 id."""
     # TODO: get average student reading duration for this page
     return random()
 
 
 def module_reading_duration(module_num: int, data448_id: int = None) -> float:
-    """in minutes, average of all students"""
+    """Returns the module reading duration (average of all page reading durations) in minutes.
+    Average of all students unless given a data_448 id. """
     # TODO: get average student reading duration for this page
     return random()
 
