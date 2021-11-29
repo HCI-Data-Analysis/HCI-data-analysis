84595e770ca80acd827e7368c784e4b521e4e98c
opeyem1a <opeyadeyemi@gmail.com>
2021-11-29 01:10:12 -0800

    + minor figure formatting

--- scripts/reading_logs/num_paragraphs_analysis.py
+++ scripts/reading_logs/num_paragraphs_analysis.py
@@ -20,7 +20,7 @@ def analyze_num_paragraphs():
                 reading_logs_data.page_reading_speed(module_num, page_num)
             )
 
-    plot_scatter(num_paragraphs_list, adjusted_reading_speeds, 'Adjusted reading speeds by page paragraph count')
+    plot_scatter(num_paragraphs_list, adjusted_reading_speeds, 'Reading speeds as a function of page paragraph count')
 
 
 def page_is_valid(module_num, page_content) -> bool:
@@ -33,6 +33,6 @@ def plot_scatter(x: [], y: [], title):
     set_plot_settings()
     plt.scatter(x, y)
     plt.xlabel("Number of Paragraphs")
-    plt.ylabel("Adjusted Reading Speed")
+    plt.ylabel("Average Class Reading Speed")
     plt.title("\n".join(wrap(f"{title}")))
     plt.show()
