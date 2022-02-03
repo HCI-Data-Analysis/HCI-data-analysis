f686f5d82562a6e4ef78e3d89c797802df65401b
opeyem1a <opeyadeyemi@gmail.com>
2022-02-02 19:22:00 -0800

    + add technique to filter excluded modules/pages

--- schemas/course.py
+++ schemas/course.py
@@ -1,8 +1,25 @@
 class CourseSchema:
+    CANCELLED_MODULES = [
+        9
+    ]
+
     OPTIONAL_MODULES = [
-        9,
         11
     ]
+
+    SUMMARY_MODULES = [
+        10
+    ]
+
+    OPTIONAL_PAGES = {
+        1: [6],
+        2: [6],
+        4: [11],
+        6: [7],
+        7: [7],
+        8: [5],
+        11: [1, 2, 3, 4, 5]  # All
+    }
 
     OPTIONAL_PAGE_TITLES = [
         'interview'
@@ -24,3 +41,20 @@ class CourseSchema:
         741752: 10,
         741753: 11
     }
+
+    OUTLIER_DATA448_IDS = [
+        # TODO: add outlier student ids
+    ]
+
+    @staticmethod
+    def page_is_valid(module_num: int, page_num: int) -> bool:
+        if module_num in CourseSchema.CANCELLED_MODULES or \
+                module_num in CourseSchema.OPTIONAL_MODULES or \
+                module_num in CourseSchema.SUMMARY_MODULES:
+            return False
+
+        if module_num in CourseSchema.OPTIONAL_PAGES and \
+                page_num in CourseSchema.OPTIONAL_PAGES[module_num]:
+            return False
+
+        return True
